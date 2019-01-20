2c5f601f8e0e68bc007c7120e2aedd475eceff8c
jon91@utexas.edu <jon91@utexas.edu>
2019-01-20 13:37:38 -0800

    final

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,24 @@
 // Set routes
 const path = require("path");
 const express = require("express");
+const router = express.Router();
 
-module.exports = function(app) {
-app.get("/", function(req, res) {
+// module.exports = function(app) {
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/home.html"));
+// });
+
+// app.get("/survey", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/survey.html"));
+// });
+// };
+
+router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
-app.get("/survey", function(req, res) {
+router.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
-};
+
+module.exports = router;
